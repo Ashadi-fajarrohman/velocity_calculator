5e2178667e7ab2d31125500b30dacf19ae069e40
ACER <fajarrohmanashadi>
2019-10-11 16:45:17 +0700

    kecepatan tanpa fungsi

---
+++ main.py
@@ -0,0 +1,11 @@
+nama = 'AshadiFajarrohman'
+program ='gerak lurus'
+
+print(f'program {program} oleh {nama}')
+
+jarak =1000
+waktu = 5 * 60
+kecepatan = jarak /waktu
+
+print(f'jarak = {jarak/1000}km ditrmpuh dalam = {waktu/60}menit')
+print(f'sehingga kecpatan = {kecepatan} m/s')
